c92e4d052b33e943d7b06c48922df908aec10bc8
GoNorman <dr.cheif-afanasyev@yandex.ru>
2023-12-29 11:37:18 +0100

    create sql request + add method in repo

--- src/main/java/com/example/reltyhubapp/repository/BuildsRepository.java
+++ src/main/java/com/example/reltyhubapp/repository/BuildsRepository.java
@@ -1,11 +1,16 @@
 package com.example.reltyhubapp.repository;
 
 import com.example.reltyhubapp.entity.Builds;
+import com.example.reltyhubapp.entity.Clients;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 public interface BuildsRepository extends JpaRepository<Builds, Long> {
 
     List<Builds> findByTitle(String title);
+    @Query("SELECT b FROM Builds b JOIN b.user u WHERE u.id = :userId")
+    List<Builds> findBuildsByUserId(@Param("userId") Long userId);
 
 }
--- src/main/java/com/example/reltyhubapp/repository/ClientRepository.java
+++ src/main/java/com/example/reltyhubapp/repository/ClientRepository.java
@@ -2,7 +2,13 @@ package com.example.reltyhubapp.repository;
 
 import com.example.reltyhubapp.entity.Clients;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ClientRepository extends JpaRepository<Clients, Long> {
+    @Query("SELECT c FROM Clients c JOIN c.user u WHERE u.id = :userId")
+    List<Clients> findClientsByUserId(@Param("userId") Long userId);
 
 }
