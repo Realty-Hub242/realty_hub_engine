fad76040e413a4947f539a18b872b76612c58834
trickle404 <m.derkaqw@gmail.com>
2025-07-30 16:53:43 +0200

    FIX : fixing a dependencis

--- src/main/java/com/example/reltyhubapp/entity/User.java
+++ src/main/java/com/example/reltyhubapp/entity/User.java
@@ -2,37 +2,54 @@ package com.example.reltyhubapp.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-
+import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "user", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"user_name"}),
-        @UniqueConstraint(columnNames = {"email"})
-})
+@Table(
+    name = "user",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = { "user_name" }),
+        @UniqueConstraint(columnNames = { "email" }),
+    }
+)
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long ID;
+
     private String name;
+
     @JsonIgnore
     private String userName;
+
     @JsonIgnore
     private String email;
+
     @JsonIgnore
     private String password;
+
     @JsonIgnore
     private String roles;
+
     private String numberPhone;
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user")
+
+    @OneToMany(
+        cascade = CascadeType.ALL,
+        fetch = FetchType.LAZY,
+        mappedBy = "user"
+    )
     private List<Clients> clientsList = new ArrayList<>();
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user")
+    @OneToMany(
+        cascade = CascadeType.ALL,
+        fetch = FetchType.LAZY,
+        mappedBy = "user"
+    )
     private List<Builds> buildsList = new ArrayList<>();
 
     public void addClientToUser(Clients clients) {
@@ -44,6 +61,4 @@ public class User {
         builds.setUser(this);
         buildsList.add(builds);
     }
-
-
 }
